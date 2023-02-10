b77d84866ae4eba3b2f208135523bc40b346c23c
yash1th <yash1th77@gmail.com>
2023-02-10 14:04:46 +0530

    first commit

---
+++ association/src/org/tnsif/bidirectional/Department.java
@@ -0,0 +1,41 @@
+package org.tnsif.bidirectional;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="department")
+public class Department {
+	
+	@Id
+	//@Column(name="DeptId")
+	private int deptid;
+	private String deptname;
+	
+	/*@OneToMany(mappedBy="department")
+	private Set<Employee>employee;*/
+	
+	public int getDeptid() {
+		return deptid;
+	}
+	public void setDeptid(int deptid) {
+		this.deptid = deptid;
+	}
+	public String getDeptname() {
+		return deptname;
+	}
+	public void setDeptname(String deptname) {
+		this.deptname = deptname;
+	}
+	
+	
+	
+	
+	
+
+}
